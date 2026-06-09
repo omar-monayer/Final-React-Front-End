4348df660b199975db0bfc8d6a70d2699b22fc86
omar <103359026+omar-monayer@users.noreply.github.com>
2026-06-10 00:56:17 +0300

    Add dashboard actions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import PagePanel from "./components/PagePanel";
-import ActionButton from "./components/ActionButton";
-import "./styles/actionbutton.css";
+import DashboardActions from "./components/DashboardActions";
 
 function Home() {
   return (
     <PagePanel title="Dashboard">
-      <div className="button-group">
-        <ActionButton>Analytics</ActionButton>
-        <ActionButton>Export to Excel</ActionButton>
-        <ActionButton>Select</ActionButton>
-      </div>
+      <DashboardActions />
     </PagePanel>
   );
 }
